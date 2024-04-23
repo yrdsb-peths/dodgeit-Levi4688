67bcc4b5e382bbeffbdc096b5a79fec5ab5015b8
Levi4688 <350647954@gapps.yrdsb.ca>
2024-04-23 16:11:03 -0400

    add hero

--- Hero.java
+++ Hero.java
@@ -14,6 +14,7 @@ public class Hero extends Actor
         if(atTop){
             setLocation(100, 100);
         } else {
-        setLocation(100, 300);
+            setLocation(100, 300);
         }
+    }
 }
