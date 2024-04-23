ee373ade74d1599de47f23aa29a2bf4c0cc5b6eb
Levi4688 <350647954@gapps.yrdsb.ca>
2024-04-23 16:21:42 -0400

    gameover

--- Banana.java
+++ Banana.java
@@ -11,6 +11,13 @@ public class Banana extends Actor
         if(getX() <= 0) {
             resetBanana();
         }
+        
+        if(isTouching(Hero.class)){
+            //add game over symbol
+            GameOver gameOver = new GameOver();
+            getWorld().addObject(gameOver, 300, 200);
+            getWorld().removeObject(this);
+        }
     }
     
     public void resetBanana(){
